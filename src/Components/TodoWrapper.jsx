ceff8fa468e0a83611590d7696b84425696a5b50
Renuma1618 <renuka1682003@gmail.com>
2025-06-27 23:48:53 +0200

    added uuid

--- src/Components/TodoWrapper.jsx
+++ src/Components/TodoWrapper.jsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Todofrom from './Todofrom'
-import { v4 as uuidv4 } from 'uuid';
-import Todo from './Todo';
-import Edit from './Edit';
-uuidv4();
+import { v4 as uuidv4 } from 'uuid'
+import Todo from './Todo'
+import Edit from './Edit'
 
 const TodoWrapper = () => {
+  const [todos, setTodos] = useState([])
 
-  const[todo,settodo]=useState([])
+  useEffect(() => {
+    console.log(todos)
+  }, [todos])
 
-  const addtodo = (todos) => {
-    settodo([...todo, {id:uuidv4(), task:todos, completed:false, isEditing:false}])
-    console.log(todo)
+  const addTodo = (task) => {
+    setTodos([...todos, { id: uuidv4(), task, completed: false, isEditing: false }])
   }
 
   const toggleComplete = (id) => {
-    settodo(todo.map(todo=>todo.id === id?{...todo, completed:!todo.completed}:todo))
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
   }
 
-  const deleteTodo = (id) =>{
-    settodo(todo.filter(todo=>todo.id !== id))
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
   }
 
   const edit = (id) => {
-    settodo(todo.map(todo=>todo.id === id?{...todo, isEditing:!todo.isEditing}:todo))
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo))
   }
 
   const editTask = (value, id) => {
-    settodo(todo.map(todo=>todo.id === id?{...todo, task: value, isEditing:!todo.isEditing}:todo))
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, task: value, isEditing: false } : todo))
   }
 
   return (
     <div className='TodoWrapper'>
       <h1>Todo List</h1>
-      <Todofrom addtodo={addtodo}/>
-      {todo.map((todos) => (
-        todos.isEditing ? (
-          <Edit edit={editTask} task={todos} key={todos.id} />
+      <Todofrom addtodo={addTodo} />
+      {todos.map(todo => (
+        todo.isEditing ? (
+          <Edit edit={editTask} task={todo} key={todo.id} />
         ) : (
           <Todo
-            task={todos}
-            key={todos.id}
+            task={todo}
+            key={todo.id}
             toggle={toggleComplete}
             deleteTodo={deleteTodo}
             edit={edit}
